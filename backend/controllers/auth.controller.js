2bae83970f07f7ba9d2f3be6e85ba905ea98859a
gundeeps247 <gundeepsinghm@gmail.com>
2024-08-09 12:13:49 +0530

    created signup backend route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+// controllers are used to define function, which can also be defined in the routes
+// the functions defined in controllers are used in routes, and routes are used in the main file
+
+
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'; 
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10);// 10 number of rounds for the salt
+    const newUser = new User({ username, email, password:hashedPassword });
+    try {
+        await newUser.save() //wait until we recieve promise
+        res.status(201).json({ message: "User registered succesfully" });
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+};
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
 .then(() => {
@@ -19,4 +22,5 @@ app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
  
-app.use('/backend/user', userRoutes)
+app.use('/backend/user', userRoutes);
+app.use('/backend/auth', authRoutes);
