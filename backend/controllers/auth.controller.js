4fb6a8b86ce44534fd18343101745bce0aa057a6
gundeeps247 <gundeepsinghm@gmail.com>
2024-08-10 12:11:21 +0530

    added a middleware and a function to handle errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,8 +4,9 @@
 
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'; 
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);// 10 number of rounds for the salt
     const newUser = new User({ username, email, password:hashedPassword });
@@ -13,6 +14,6 @@ export const signup = async (req, res) => {
         await newUser.save() //wait until we recieve promise
         res.status(201).json({ message: "User registered succesfully" });
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,26 @@ const app = express();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log("Connected to MongoDb");
-})
-.catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-});
+    .then(() => {
+        console.log("Connected to MongoDb");
+    })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+    });
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
- 
+
 app.use('/backend/user', userRoutes);
 app.use('/backend/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    })
+})
