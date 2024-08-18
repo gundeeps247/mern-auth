2bf0a42ef6a1cc28694edc0b63a15be297072978
gundeeps247 <gundeepsinghm@gmail.com>
2024-08-18 18:06:01 +0530

    completed home and about page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to Health Matrix</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste facere voluptatibus laboriosam ipsam, aliquam asperiores quia distinctio, dignissimos quas vero perspiciatis dolorem. Molestias tempora quidem dolore qui in, odio sed?</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste facere voluptatibus laboriosam ipsam, aliquam asperiores quia distinctio, dignissimos quas vero perspiciatis dolorem. Molestias tempora quidem dolore qui in, odio sed?</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste facere voluptatibus laboriosam ipsam, aliquam asperiores quia distinctio, dignissimos quas vero perspiciatis dolorem. Molestias tempora quidem dolore qui in, odio sed?</p>
+    </div>
   )
 }
