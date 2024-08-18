69ad6b6b8d4e38aef1711f3042409a07de19b643
gundeeps247 <gundeepsinghm@gmail.com>
2024-08-18 18:15:50 +0530

    added changes for deploying(know as pre production changes)

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,9 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
-
-const app = express();
-
-app.use(cookieParser());
-app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
     .then(() => {
@@ -23,6 +19,20 @@ mongoose.connect(process.env.MONGO)
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'))
+})
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/backend/user', userRoutes);
 app.use('/backend/auth', authRoutes);
