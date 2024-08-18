d7c117373e3cf80e62af0f3a222b86d5eb1128f4
gundeeps247 <gundeepsinghm@gmail.com>
2024-08-18 14:38:57 +0530

    created update user backend route

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
