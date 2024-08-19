d86226b9cf976f8449cf5379873219e6c5312556
gundeeps247 <gundeepsinghm@gmail.com>
2024-08-19 19:26:57 +0530

    deploying on render

--- backend/index.js
+++ backend/index.js
@@ -4,41 +4,45 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
 
 const app = express();
 
-app.use(cookieParser());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected to MongoDb");
-    })
-    .catch((err) => {
-        console.error("Failed to connect to MongoDB", err);
-    });
-
-app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-// Middleware and routes
 app.use(express.json());
+
 app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
 
 app.use('/backend/user', userRoutes);
 app.use('/backend/auth', authRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        statusCode,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
